e003215f563079c45fa4f8d6e590c4bebc56ad7e
omegaelit <omegaelit@yahoo.com>
2022-12-18 02:45:01 +0300

    Add ProjectDeleteForm

--- src/Forms/deleteForms/ProjectDeleteForm.cpp
+++ src/Forms/deleteForms/ProjectDeleteForm.cpp
@@ -6,7 +6,8 @@
 #include <string>
 #include <set>
 #include <vector>
-#include <Forms/imguidatechooser/imguidatechooser.hPP>
+
+#include <Core/FormHandler.hpp>
 
 namespace DataGraph::Forms
 {
@@ -14,8 +15,8 @@ int ProjectDelete::init() { return 0; }
 
 int ProjectDelete::draw()
 {
-	char nameBuf[20] = {};
-	int maxSize = 10;
+	static int curProjectId = -1;
+	static int curNodeId = -1;
 
 	if (!m_errorMessage.empty())
 	{
@@ -25,43 +26,98 @@ int ProjectDelete::draw()
 	{
 		ImGui::Text("Project name: ");
 		ImGui::SameLine();
-		int curItem = 0;
-		int itemCount = 2;
-		const char* items[] = {"Sberbank", "Vkontakte", "Facebook"};
-		ImGui::PushItemWidth(350);
-		ImGui::Combo("##ProjectNameList", &curItem, items, itemCount);
+		if (ImGui::BeginCombo("##ProjectNameList", m_projectName.c_str()))
+		{
+			auto* prjName = &m_projectName;
+			FormHandler::getDbConn()->execute(
+				 [prjName](auto&& data) {
+					 using dmitigr::pgfe::to;
+					 std::pair dt{to<int>(data[0]), std::move(to<std::string>(data[1]))};
+					 if (ImGui::Selectable(dt.second.c_str()))
+					 {
+						 curProjectId = dt.first;
+						 *prjName = std::move(dt.second);
+					 }
+				 },
+				 "SELECT * FROM getProjectListNames($1)", m_projectNameSearch);
+			ImGui::EndCombo();
+		}
 
-		std::string projectSearch;
 		ImGui::SameLine();
 		ImGui::Text("Project search by name: ");
 		ImGui::SameLine();
-		ImGui::PushItemWidth(350);
-		ImGui::InputText("##ProjectNameSearch", &projectSearch);
+		ImGui::PushItemWidth(250);
+		ImGui::InputText("##ProjectNameSearch", &m_projectNameSearch);
 	}
 
-	ImGui::Text("Project Version: ");
-	ImGui::SameLine();
+	if (!m_projectName.empty())
 	{
-		int curItem = 0;
-		int itemCount = 2;
-		const char* items[] = {"1.0", "1.01", "2.2f"};
-		ImGui::PushItemWidth(350);
-		ImGui::Combo("##ProjectVersionList", &curItem, items, itemCount);
-	}
+		ImGui::Text("Project Version: ");
+		ImGui::SameLine();
+		if (ImGui::BeginCombo("##ProjectVersionList", m_projectVersion.c_str()))
+		{
+			auto* prjVersion = &m_projectVersion;
+			try
+			{
+				FormHandler::getDbConn()->execute(
+					 [prjVersion](auto&& data) {
+						 using dmitigr::pgfe::to;
+						 std::pair dt{to<int>(data["nodeid"]), std::move(to<std::string>(data["version"]))};
+						 if (ImGui::Selectable(dt.second.c_str()))
+						 {
+							 curNodeId = dt.first;
+							 *prjVersion = std::move(dt.second);
+						 }
+					 },
+					 "SELECT * FROM getprojects2(searchprojectid=>$1, namesearch=>$2, searchversion=>$3)", curProjectId, m_projectName,
+					 m_projectVersionSearch);
+			}
+			catch (std::exception& ex)
+			{
+				std::cout << ex.what();
+			}
+			ImGui::EndCombo();
+		}
 
-	std::string projectVersionSearch;
-	ImGui::SameLine();
-	ImGui::Text("Project version search: ");
-	ImGui::SameLine();
-	ImGui::PushItemWidth(350);
-	ImGui::InputText("##ProjectVersionSearch", &projectVersionSearch);
+		ImGui::SameLine();
+		ImGui::Text("Project version search: ");
+		ImGui::SameLine();
+		ImGui::PushItemWidth(350);
+		ImGui::InputText("##ProjectVersionSearch", &m_projectVersionSearch);
+	}
 
 	ImGui::PushStyleColor(ImGuiCol_Button, {0.6, 0, 0, 1});
 	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, {0.2, 0, 0, 1});
-	if (ImGui::Button("Submit"))
+	if (!m_projectVersion.empty() && ImGui::Button("Submit"))
 	{
-		m_errorMessage = "No database connection";
-		ImGui::OpenPopup("Help");
+		uint32_t* errorCode = &m_errorCode;
+		FormHandler::getDbConn()->execute(
+			 [errorCode](auto&& data) {
+				 using dmitigr::pgfe::to;
+				 *errorCode = to<int>(data[0]);
+			 },
+			 "SELECT * FROM deleteproject(nodeid=>$1) LIMIT 1", curNodeId);
+
+		switch (m_errorCode)
+		{
+		case 0:
+			m_projectName = "";
+			m_projectVersion = "";
+			curNodeId = -1;
+			curProjectId = -1;
+			m_errorMessage = "Value has been successfully deleted";
+			break;
+		case 1:
+			m_projectName = "";
+			m_projectVersion = "";
+			curNodeId = -1;
+			curProjectId = -1;
+			m_errorMessage = "Ivalid value selected, maybe it was removed or updated earlier, please try again.";
+			break;
+		case 2:
+			ImGui::OpenPopup("Help");
+			break;
+		}
 	}
 
 	bool windowOpen = true;
@@ -89,7 +145,35 @@ int ProjectDelete::draw()
 		ImGui::SetCursorPosX(popupSize.x - ImGui::CalcTextSize("Agree").x - ImGui::GetStyle().WindowPadding.x - 5);
 
 		if (ImGui::Button("Agree"))
-		{}
+		{
+			uint32_t* errorCode = &m_errorCode;
+			FormHandler::getDbConn()->execute(
+				 [errorCode](auto&& data) {
+					 using dmitigr::pgfe::to;
+					 *errorCode = to<int>(data[0]);
+				 },
+				 "SELECT * FROM deleteproject(nodeid=>$1, force=>TRUE) LIMIT 1", curNodeId);
+
+			switch (m_errorCode)
+			{
+			case 0:
+				m_projectName = "";
+				m_projectVersion = "";
+				curNodeId = -1;
+				curProjectId = -1;
+				m_errorMessage = "Value has been successfully deleted";
+				break;
+			case 1:
+				m_projectName = "";
+				m_projectVersion = "";
+				curNodeId = -1;
+				curProjectId = -1;
+				m_errorMessage = "Ivalid value selected, maybe it was removed or updated earlier, please try again.";
+				break;
+			}
+
+			ImGui::CloseCurrentPopup();
+		}
 
 		ImGui::EndPopup();
 	}
@@ -100,8 +184,6 @@ int ProjectDelete::draw()
 
 const char* ProjectDelete::name() const { return "Projects"; }
 
-void ProjectDelete::reset()
-{
-}
+void ProjectDelete::reset() {}
 
 }  // namespace DataGraph::Forms
